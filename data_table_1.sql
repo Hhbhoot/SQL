abed667f86383df6b589055bdfbe8bb4f878e97e
Hitesh <hiteshbhut666@gmail.com>
2023-10-03 17:56:22 +0530

    SQL

---
+++ data_table_1.sql
@@ -0,0 +1,48 @@
+-- \sql
+-- connect root@localhost:3306
+
+-- create databases;
+
+-- use database hitesh
+
+--  create table CLIENT_MASTER(
+--     clientNO varchar(6),
+--     Name varchar(20),
+--     ADDRESS1 varchar(20),
+--     ADDRESS2 varchar(20),
+--     city varchar(20),
+--     PinCode int(8),
+--     State varchar(15),
+--     baldue int(15)
+
+
+-- );
+
+-- insert into CLIENT_MASTER(
+
+--     clientNO,Name,ADDRESS1,ADDRESS2,city,PinCode,State,baldue
+-- ) values(
+--     "C00001","Hitesh","Bhavnagar","TaLAJA","Bhavnagar","395008","Gujarat",150000
+-- )
+
+-- insert into CLIENT_MASTER values(
+
+--     clientNO,Name,ADDRESS1,ADDRESS2,city,PinCode,State,baldue
+-- )values(
+--     "C00002","Mamta Muzmudar","A-104","worli","Mumbai",400002,"Maharashtra",12000
+-- );
+
+-- insert into CLIENT_MASTER(
+
+--  "C00003","Chhaya Bankar","B-104","Nariman","Mumbai",400001,"Maharashtra",14000
+
+-- )
+
+-- insert into CLIENT_MASTER values(
+
+--     "C00004","Ashwini joshi","C-104","Bandra","Bangalore",560001,"Karnataka",5000
+-- ),(
+--     "C00005","Deepak Sharma","D-104","Kapodra","Mangalore",560050,"Karnataka",0
+-- ),(
+--     "C00006","Hansel Colaco","E-104","Hirabaugh","Mumbai",400060,"Maharashtra",2000
+-- )
